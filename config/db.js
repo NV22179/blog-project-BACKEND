654954638cd906d5fcc48c4c3836259b04ebe3e8
NV22179 <147533054+NV22179@users.noreply.github.com>
2024-03-25 17:45:53 +0000

    13 - Connecting node.js to database

--- config/db.js
+++ config/db.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DB_URL);
-        console.log('Database is connected....')
-    } catch (error){
-        console.log(`Error: ${error.message}`);
-        process.exit(1);
-    }
-
+  try {
+    await mongoose.connect(process.env.DB_URI);
+    console.log("Database is connected...");
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
--- server.js
+++ server.js
@@ -8,9 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-
-    res.send("Server is running...")
-
+  res.send("Server is running...");
 });
 
 const PORT = process.env.PORT || 5000;

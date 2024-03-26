f480581d96b1acc227a4ad38e6055bfac6d2958e
NV22179 <147533054+NV22179@users.noreply.github.com>
2024-03-26 20:22:53 +0000

    18 - fixing bugs

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,13 @@
-import express from "express";
+import express from "express"
 const router = express.Router();
-import { registerUser } from "../controllers/userControllers";
-import {
-  registerUser,
-  loginUser,
-  userProfile,
-} from "../controllers/userControllers";
+import { registerUser, loginUser, userProfile, updateProfile, updateProfilePicture } from "../controllers/userControllers"
 import { authGuard } from "../middleware/authMiddleware";
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-router.get("/profile", authGuard, userProfile);
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/profile', authGuard , userProfile);
+router.put('/updateProfile', authGuard, updateProfile)
+router.put('/updateProfilePicture', authGuard, updateProfilePicture)
 
 export default router;

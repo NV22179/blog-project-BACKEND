7322f9e6be3c68c42795beb109d7012ba4a3e2e5
NV22179 <147533054+NV22179@users.noreply.github.com>
2024-03-26 07:14:05 +0000

    16 - Login & User profile

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,25 +1,64 @@
-import User from "../models/User";
+let user = await User.findOne({ email });
 
-export const registerUser = async (req, res) => {
-export const registerUser = async (req, res, next) => {
-  try {
-    const { name, email, password } = req.body;
+if (user) {
+  // return res.status(400).json({ message: "User have already registered" });
+  throw new Error("User have already registered");
+}
 
-    // check whether the user exists or not
-    let user = await User.findOne({ email });
-
-    if (user) {
-      return res.status(400).json({ message: "User have already registered" });
-      // return res.status(400).json({ message: "User have already registered" });
-      throw new Error("User have already registered");
-    }
-
-    // creating a new user
-@@ -28,7 +29,7 @@ export const registerUser = async (req, res) => {
-      token: await user.generateJWT(),
-    });
-  } catch (error) {
-    return res.status(500).json({ message: "Something went wrong!" });
-    next(error);
-  }
+@@ -33,4 +32,55 @@ export const registerUser = async (req, res, next) => {
+}
 };
+
+export { registerUser };
+export const loginUser = async (req, res, next) => {
+try {
+const { email, password } = req.body;
+
+let user = await User.findOne({ email });
+
+if (!user) {
+  throw new Error("Email not found");
+}
+
+if (await user.comparePassword(password)) {
+  return res.status(201).json({
+    _id: user._id,
+    avatar: user.avatar,
+    name: user.name,
+    email: user.email,
+    verified: user.verified,
+    admin: user.admin,
+    token: await user.generateJWT(),
+  });
+} else {
+  throw new Error("Invalid email or password");
+}
+} catch (error) {
+next(error);
+}
+};
+
+export const userProfile = async (req, res, next) => {
+try {
+let user = await User.findById(req.user._id);
+
+if (user) {
+  return res.status(201).json({
+    _id: user._id,
+    avatar: user.avatar,
+    name: user.name,
+    email: user.email,
+    verified: user.verified,
+    admin: user.admin,
+  });
+} else {
+  let error = new Error("User not found");
+  error.statusCode = 404;
+  next(error);
+}
+} catch (error) {
+next(error);
+}
+};
+
+export { registerUser, loginUser };

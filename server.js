ed85ec146eff22826fe17a032dfa3f23519ee27d
NV22179 <147533054+NV22179@users.noreply.github.com>
2024-05-21 12:53:06 +0000

    31 - create & update post endpoint

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import connectDB from "./config/db";
+import {
+  errorResponserHandler,
+  invalidPathHandler,
+} from "./middleware/errorHandler";
+
+// Routes
+import userRoutes from "./routes/userRoutes";
+import postRoutes from "./routes/postRoutes";
 
 dotenv.config();
 connectDB();
@@ -10,6 +19,15 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server is running...");
 });
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+
+// static assets
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+app.use(invalidPathHandler);
+app.use(errorResponserHandler);
 
 const PORT = process.env.PORT || 5000;
 
